cc2b1ec905febb84332450b0fff695e7347096e0
Ruton Code <91872152+RutonC@users.noreply.github.com>
2023-11-22 02:00:26 +0200

    :lipstick: Finish a Onboarding Screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import Onboarding from "../screens/Onboarding";
+import Home from "../screens/Home";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,6 +15,9 @@ export default function Routes() {
         <Stack.Screen name="onboarding"  component={Onboarding} options={{
           headerShown:false
         }}/>
+        <Stack.Screen name="Home" component={Home} options={{
+          headerShown:false
+        }} />
       </Stack.Group>
     </Stack.Navigator>
   )
--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -1,19 +1,79 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import React from "react";
+import { color } from "../constants/colors";
+import Medicines from "../assets/svg/Onboarding_1.svg";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import { useNavigation } from "@react-navigation/native";
 
 export default function Onboarding() {
+
+  const {navigate} = useNavigation();
   return (
     <View style={styles.container}>
-      <Text>Onboarding</Text>
+      <Medicines height={231.63} width={327} />
+      <View style={styles.card}>
+        <Text style={styles.cartTitle}>Discover Pharmacies in Your Region and Find Your Medicines!</Text>
+        <Text style={styles.cardDesc}>Explore nearby pharmacies using our interactive map. Quickly find the most convenient options for your needs.</Text>
+
+        <View style={styles.cardFooter}>
+          <TouchableOpacity>
+            <Text style={styles.buttonTextSkip}>Skip</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={()=>navigate('Home')}>
+            <MaterialCommunityIcons name="arrow-right" size={27} color={color.white}/>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: color.primary,
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 24,
   },
+  card: {
+    height: 302,
+    width: "100%",
+    backgroundColor: color.white,
+    padding: 21,
+    borderRadius: 9,
+    marginTop: 102,
+  },
+  cartTitle:{
+    fontFamily:'roboto-bold',
+    fontSize:26,
+    color:color.secondary
+  },
+  cardDesc:{
+    fontFamily:'roboto',
+    fontSize:16,
+    color:color.gray,
+    // textAlign:"center",
+    marginTop:6
+  },
+  cardFooter:{
+    marginTop:41,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  button:{
+    height:58,
+    width:58,
+    backgroundColor:color.primary,
+    borderRadius:58,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  buttonTextSkip:{
+    fontFamily:'roboto',
+    fontSize:16,
+    fontWeight:'600'
+  }
+
 });
